1bff61ddc1b50ee544cf78e47ce889a8ddab2095
Matyáš Kolek <xkolek1@fi.muni.cz>
2025-10-30 22:33:48 +0100

    index OnePan only, user.login b4 id search, search and login by entering, deleted other companies

--- dpp_app/src/main.py
+++ dpp_app/src/main.py
@@ -3,34 +3,27 @@ import os
 import lib
 import global_state_lib as gsl
 
-# Load the global state
-
+# global state is always ONEPan
 st.set_page_config(page_title="Digital Product Passport", page_icon=":material/edit:", layout = 'wide')
 global_state = gsl.get_global_state()
+global_state['company'] = 'ONEPan'
 
+# always show product ID and user login pages in the sidebar
+pages = []
 
-if 'company' in global_state.get_key_names():
-    pages = []
-    pages.append(st.Page("web_pages/product_id.py", title="Enter Product ID", icon=":material/verified_user:"))
-    pages.append(st.Page("web_pages/select_company.py", title="Select Company", icon=":material/enterprise:"))
-    #Get the name of the logo image
-    #list files in the directory
-    path_image = os.path.join(os.getcwd(),os.path.join("images", global_state['company']))
-    files = [f for f in os.listdir(path_image) if os.path.isfile(os.path.join(path_image, f)) and 'logo'in str(f)]
-    st.sidebar.image(os.path.join(path_image,files[0]), caption=global_state['company'])
-    global_state['pages_loaded'] = False
-else:
-    pages = []
-    pages.append(st.Page("web_pages/select_company.py", title="Select Company", icon=":material/enterprise:"))
-    global_state['pages_loaded'] = False
+# get the name of the logo image - ONEPan
+# list files in the directory
+path_image = os.path.join(os.getcwd(),os.path.join("images", global_state['company']))
+files = [f for f in os.listdir(path_image) if os.path.isfile(os.path.join(path_image, f)) and 'logo'in str(f)]
+st.sidebar.image(os.path.join(path_image,files[0]), caption=global_state['company'])
+global_state['pages_loaded'] = False
 
 #https://your_app.streamlit.app/?first_key=1&second_key=two&third_key=true
-#http://172.25.113.64:8501/?Company=Brighteco&Product_ID=1
+#http://172.25.113.64:8501/?Company=OnePan&Product_ID=1
 
 if 'Product_ID' in st.query_params and not('request' in st.session_state.keys()):    
     st.session_state['request'] = False       
     product_id = st.query_params['Product_ID']
-    global_state['company'] = st.query_params.get('Company', 'Brighteco')
     fd = lib.communication()
     fd.fetch_data(product_id)
 
@@ -39,17 +32,22 @@ if global_state['status'] == 'OK':
                           for category in global_state['metadata_data_content_pages'].keys())
     lib.create_generic_page_py(function_names)    
     i = 0
-    pages = []
+
     for page in global_state['metadata_data_content_pages'].keys():
                   
         pages.append(st.Page('web_pages/'+function_names[i]+'.py', title=page, 
                             icon=global_state['metadata_data_content_pages'][page]['icon_category']))                  
         i = i + 1
-    pages.append(st.Page("web_pages/user_login.py", title="User login", icon=":material/passkey:"))
-    pages.append(st.Page("web_pages/product_id.py", title="Enter Product ID", icon=":material/verified_user:"))
-    pages.append(st.Page("web_pages/select_company.py", title="Select Company", icon=":material/enterprise:"))
     global_state['pages_loaded'] = True
 
+pages.append(st.Page("web_pages/product_id.py", title="Enter Product ID", icon=":material/verified_user:"))
+
+# Determine the title for user login page
+login_title = "User Login"
+if 'Login' in global_state.get_key_names() and global_state['Login'] and 'username' in global_state.get_key_names():
+    login_title = global_state['username']
+
+pages.append(st.Page("web_pages/user_login.py", title=login_title, icon=":material/passkey:"))
         
 pg = st.navigation(pages)   
 global_state['current_page_name'] = pg.title if pg.title != '' else 'Home'
